548aaaa9bcdc8f821c2e74a9e1893f70915988e6
Olutunde <solabi.tunde@gmail.com>
2022-02-07 09:43:56 +0000

    fix: qrocode reader size

--- src/components/QRModal.jsx
+++ src/components/QRModal.jsx
@@ -103,7 +103,6 @@ const QRModal = ({ modal, onModalClose, getClasses, classId }) => {
                     </div>{" "}
                   </div>
                 ) : null}
-                <div className="sm:flex sm:items-start">
                   <div className="mt-3 text-center sm:mt-0 mt-2 sm:ml-4 sm:text-left">
                     <Dialog.Title
                       as="h3"
@@ -111,18 +110,17 @@ const QRModal = ({ modal, onModalClose, getClasses, classId }) => {
                     >
                       Scan QR Code
                     </Dialog.Title>
-                    <div className="mt-4 h-40">
+                    <div className="mt-4 h-96 w-11/12">
                       <QrReader
                         delay={300}
-                        style={{ width: "100%" }}
                         onError={handleError}
                         onScan={handleScan}
+                        style={{ width: "100%" }}
                       />
                     </div>
-                  </div>
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <div className="bg-gray-50 px-4 py-3 mt-4 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
                   className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
